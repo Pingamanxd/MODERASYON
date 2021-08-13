c125fcbe1b6dbbc18dc6f07bb59ecb226732e840
Glitch (canary-repeated-channel) <none>
2021-08-13 01:50:22 +0000

    Import of Wrolmia/ekip-moderation

--- Source/Schema/NameData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const NameData = mongoose.Schema({
-  UserID: String,
-  LastName: String
-})
-
-module.exports = mongoose.model("NameData", NameData)
--- Source/Schema/UserData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserData = mongoose.Schema({
-  UserID: String,
-  Name: String,
-  Process: String
-})
-
-module.exports = mongoose.model("UserData", UserData)
