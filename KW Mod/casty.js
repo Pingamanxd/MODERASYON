bae7eca8921777c66e870b5cd372ff41d856b978
Glitch (canary-repeated-channel) <none>
2021-08-13 01:59:47 +0000

    🏢💈 Checkpoint ./KW Mod/casty.js:32005045/30

--- KW Mod/casty.js
+++ KW Mod/casty.js
@@ -115,7 +115,7 @@ client.on('error', e => {
     console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(process.env.token);
 
 //------------------------------------------------------------------------------------------------------------\\
 client.on("messageDelete", async message => {
